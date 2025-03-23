43f85efec9a7c175615bff3c2fa92dce29681612
admin <admin@admins-MacBook-Pro.local>
2025-03-23 02:08:42 +0200

    Add Vercel analytics

--- gear-analysis-ui/app/layout.tsx
+++ gear-analysis-ui/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from '@vercel/analytics/react';
 
 import "./globals.css";
 
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster position="top-right" reverseOrder={false} />
+        <Analytics />
       </body>
     </html>
   );
